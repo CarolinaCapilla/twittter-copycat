e391bc5ccfaed416468caddf5d2f5dac4127f91b
CarolinaCapilla <carolina.capilla@gmail.com>
2020-10-11 21:55:13 +0100

    gem setup + dependencies

--- Gemfile
+++ Gemfile
@@ -25,6 +25,12 @@ gem 'turbolinks', '~> 5'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+# Custom
+gem 'simple_form', '~> 5.0', '>= 5.0.3'
+gem 'bulma-rails', '~> 0.6.1'
+gem 'gravatar_image_tag', '~> 1.2'
+gem 'devise', '~> 4.7', '>= 4.7.3'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
@@ -40,6 +46,9 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'better_errors', '~> 2.8', '>= 2.8.3'
+  gem 'guard', '~> 2.16', '>= 2.16.2'
+  gem 'guard-livereload', '~> 2.5', require: false
 end
 
 group :test do
---
+++ app/controllers/tweeets_controller.rb
@@ -0,0 +1,50 @@
+class TweeetsController < ApplicationController
+  before_action :set_tweeet, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tweeets = Tweeet.all
+  end
+
+  def show
+  end
+
+  def new
+    @tweeet = Tweeet.new
+  end
+
+  def edit
+  end
+
+  def create
+    @tweeet = Tweeet.new(tweeet_params)
+
+    if @tweeet.save
+      redirect_to @tweeet, notice: 'Tweeet was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+  def update
+    if @tweeet.update(tweeet_params)
+      redirect_to @tweeet, notice: 'Tweeet was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweeet.destroy
+    redirect_to tweeets_url, notice: 'Tweeet was successfully destroyed.'
+  end
+
+  private
+    def set_tweeet
+      @tweeet = Tweeet.find(params[:id])
+    end
+
+    def tweeet_params
+      params.require(:tweeet).permit(:tweeet)
+    end
+end
